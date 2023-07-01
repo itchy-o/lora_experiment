296a5bfb49054a118a299acb04e9d90e36fb7d0a
Mike Weiblen <mew@mew.cx>
2023-06-30 20:01:27 -0600

    fs reorg

--- code.py
+++ code.py
@@ -1,5 +1,31 @@
-# code.py
+# test1.py - packet reliablity test
+
+import time
 import os
-os.chdir('test1')
-import test1
+
+
+f1 = open("test1.log", "a")
+f1.write("start {\n")
+f1.write("}\n")
+os.sync()
+
+
+
+
+
+def main():
+    try:
+        # if flash is writable, send results to file then reboot
+        filename = "{}__{}.txt".format(board.board_id, AsciiHex(soc.cpu.uid))
+        with open(filename, "w") as fh:
+            GenerateResults(fh)
+        soc.reset()
+    except:
+        # if flash is read-only, send results to stdout
+        GenerateResults(sys.stdout)
+
+# make it so ################################################################
+
+main()
+
 
---
+++ code_save.py
@@ -0,0 +1,5 @@
+# code.py
+import os
+os.chdir('test1')
+import test1
+
